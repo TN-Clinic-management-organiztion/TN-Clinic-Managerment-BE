e76cdea0158ef2a41ca20025ca2b62826a6fe4c5
Binkeurss <nvbinh271104@gmail.com>
2025-12-15 10:44:42 +0700

    hotfix/room_org

--- src/modules/system/org_room/dto/update-room.dto.ts
+++
@@ -1,4 +0,0 @@
-import { PartialType } from '@nestjs/mapped-types';
-import { CreateOrgRoomDto } from 'src/modules/system/org_room/dto/create-room.dto';
-
-export class UpdateOrgRoomDto extends PartialType(CreateOrgRoomDto) {}
--- src/modules/system/org_room/org-room.module.ts
+++ src/modules/system/org_room/org-room.module.ts
@@ -1,8 +1,8 @@
 import { Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
-import { OrgRoom } from 'src/database/entities/auth/org_rooms.entity';
-import { OrgRoomsController } from 'src/modules/system/org_room/org-room.controller';
-import { OrgRoomsService } from 'src/modules/system/org_room/org-room.service';
+import { OrgRoomsService } from './org-room.service';
+import { OrgRoomsController } from './org-room.controller';
+import { OrgRoom } from '../../../database/entities/auth/org_rooms.entity';
 
 @Module({
   imports: [TypeOrmModule.forFeature([OrgRoom])],
